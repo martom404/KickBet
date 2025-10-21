9ac14883db9458e43c23f1f172bbca70f0daccad
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-10-21 15:50:16 +0200

    step number 0 - new project

---
+++ src/main/java/com/mt/KickBet/KickBetApplication.java
@@ -0,0 +1,13 @@
+package com.mt.KickBet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickBetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mt/KickBet/KickBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.KickBet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
