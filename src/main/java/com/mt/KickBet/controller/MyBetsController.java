ad2a55a20e07d7709bfef007f6228856e5d885f4
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-11-04 16:46:09 +0100

    MyBets + Ranking, controller and view

---
+++ src/main/java/com/mt/KickBet/controller/MyBetsController.java
@@ -0,0 +1,22 @@
+package com.mt.KickBet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/my-bets")
+public class MyBetsController {
+
+    @GetMapping
+    public String showMyBets(Model model) {
+        // TODO: Po implementacji logowania pobierać zakłady zalogowanego użytkownika
+        // model.addAttribute("bets", betService.getBetsByUserId(currentUserId));
+        return "my_bets";
+    }
+}
+
+
+
+
---
+++ src/main/java/com/mt/KickBet/controller/RankingController.java
@@ -0,0 +1,35 @@
+package com.mt.KickBet.controller;
+
+
+import com.mt.KickBet.model.dao.User;
+import com.mt.KickBet.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Controller
+@RequestMapping("/ranking")
+public class RankingController {
+
+    private final UserService userService;
+
+    public RankingController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String showRanking(Model model) {
+        List<User> users = userService.getAllUsers();
+        users.sort(Comparator.comparingInt(User::getPoints).reversed());
+        model.addAttribute("users", users);
+        return "ranking";
+    }
+}
+
+
+
+
