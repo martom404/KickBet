76fb924d304be1060f8d95dc9d9edac4ac980ed9
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-11-09 15:52:41 +0100

    match list view + controller

---
+++ src/main/java/com/mt/KickBet/controller/MatchController.java
@@ -0,0 +1,32 @@
+package com.mt.KickBet.controller;
+
+import com.mt.KickBet.repository.MatchRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.mt.KickBet.service.MatchService;
+
+
+@Controller
+@RequestMapping("/matches")
+public class MatchController {
+
+    private final MatchService matchService;
+
+    public MatchController(MatchService matchService, MatchRepository matchRepository) {
+        this.matchService = matchService;
+    }
+
+    @GetMapping
+    public String showMatches(Model model) {
+        model.addAttribute("matches", matchService.getAllMatches());
+        return "matches";
+    }
+}
+
+
+
+
+
+
