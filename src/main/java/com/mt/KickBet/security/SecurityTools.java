5cd7cb63ab29255d6627fa65dc9840d96311ac3c
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-11-11 22:04:31 +0100

    authethication++

---
+++ src/main/java/com/mt/KickBet/security/SecurityTools.java
@@ -0,0 +1,30 @@
+package com.mt.KickBet.security;
+
+import com.mt.KickBet.model.dao.Role;
+import com.mt.KickBet.model.dao.User;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
+
+public class SecurityTools {
+    public static User getUser() {
+        SecurityContext sc = SecurityContextHolder.getContext();
+        if (sc != null
+                && sc.getAuthentication() != null
+                && sc.getAuthentication().getPrincipal() instanceof User) {
+            return (User) sc.getAuthentication().getPrincipal();
+        }
+        return null;
+    }
+
+    public static Boolean hasRole(Role role) {
+        if (getUser() == null) return false;
+        return role.equals(getUser().getRole());
+    }
+
+    public static Boolean hasRole(List<Role> roles) {
+        if (getUser() == null) return false;
+        return roles.contains(getUser().getRole());
+    }
+}
