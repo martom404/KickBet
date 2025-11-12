7f32a213a4c14f41a760ff68392ea1aeb775009c
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-11-12 10:24:51 +0100

    api connection dto/apiconfig

---
+++ src/main/java/com/mt/KickBet/model/dto/football/FootballApiMatch.java
@@ -0,0 +1,52 @@
+package com.mt.KickBet.model.dto.football;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FootballApiMatch {
+    
+    private Long id;
+    
+    @JsonProperty("homeTeam")
+    private Team homeTeam;
+    
+    @JsonProperty("awayTeam")
+    private Team awayTeam;
+    
+    @JsonProperty("utcDate")
+    private String utcDate;
+    
+    private String status;
+    
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Team {
+        private String name;
+    }
+    
+    public LocalDateTime getStartTime() {
+        if (utcDate == null || utcDate.isEmpty()) {
+            return null;
+        }
+        try {
+            return LocalDateTime.parse(utcDate.replace("Z", ""), 
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    public String getHomeTeamName() {
+        return homeTeam != null ? homeTeam.getName() : null;
+    }
+    
+    public String getAwayTeamName() {
+        return awayTeam != null ? awayTeam.getName() : null;
+    }
+}
---
+++ src/main/java/com/mt/KickBet/model/dto/football/FootballApiResponse.java
@@ -0,0 +1,15 @@
+package com.mt.KickBet.model.dto.football;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FootballApiResponse {
+    
+    @JsonProperty("matches")
+    private List<FootballApiMatch> matches;
+}
