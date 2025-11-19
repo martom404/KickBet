4a847bb75c5c58a1fff5a3d6788a4e670e122286
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-11-19 13:51:51 +0100

    mybets update

--- src/main/java/com/mt/KickBet/controller/MyBetsController.java
+++ src/main/java/com/mt/KickBet/controller/MyBetsController.java
@@ -1,5 +1,8 @@
 package com.mt.KickBet.controller;
 
+import com.mt.KickBet.model.entity.User;
+import com.mt.KickBet.security.SecurityTools;
+import com.mt.KickBet.service.BetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/my-bets")
 public class MyBetsController {
 
+    private final BetService betService;
+
+    public MyBetsController(BetService betService) {
+        this.betService = betService;
+    }
+
     @GetMapping
     public String showMyBets(Model model) {
+        User currentUser = SecurityTools.getUser();
+        if (currentUser != null) {
+            model.addAttribute("bets", betService.getUserBets(currentUser.getId()));
+        }
         return "user/my_bets";
     }
 }
